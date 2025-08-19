4f17520630a70f05ff71345b8569662a72dfd035
imoz070493 <imoz070493@gmail.com>
2025-08-19 00:57:09 -0500

    Correcion de errores

--- public/js/app.js
+++ public/js/app.js
@@ -106920,7 +106920,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\carneUniversitarioTesis\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\carneUniversitarioTesisOficial\resources\js\app.js */"./resources/js/app.js");
 
 
 /***/ })
