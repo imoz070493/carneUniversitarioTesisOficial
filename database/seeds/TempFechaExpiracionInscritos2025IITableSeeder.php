3f3bcb7153111f5876b848f3d43953bfd3ff5554
imoz070493 <imoz070493@gmail.com>
2025-10-19 22:01:50 -0500

    Correcion de errores

---
+++ database/seeds/TempFechaExpiracionInscritos2025IITableSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class TempFechaExpiracionInscritos2025IITableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $data = static::data();
+        $datos = [];
+        foreach ($data as $key => $value) {
+            $fecha_expiracion = $value['fecha_expiracion']?\Carbon\Carbon::parse($value['fecha_expiracion']):null;
+            DB::table('inscritos')
+                        ->where('codigo_estudiante',$value['codigo_estudiante'])
+                        ->where('convocatoria_id','10001')
+                        ->update(['fecha_expiracion' => $fecha_expiracion]);
+
+            // unset($datos);
+        }
+    }
+
+    public static function data(){
+        // Ruta al archivo JSON
+        $json = File::get(database_path('data/fecha_expiracion_inscritos_2025_II.json'));
+
+        // Convertir en array
+        return json_decode($json, true);
+    }
+}
